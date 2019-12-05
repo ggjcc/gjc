7d3913b0428b4c5a36976fc666c5622350649836
张家鑫 <1476535136@qq.com>
2019-12-05 09:33:10 +0800

    555

--- src/gjc/dgjj.java
+++ src/gjc/dgjj.java
@@ -5,10 +5,11 @@ public class dgjj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(1111);
+		System.out.println(1111);                                         
 		System.out.println(2222);
 		System.out.println(333);
 		System.out.println(4444);
+		System.out.println(55555);
 	}
 
 }
